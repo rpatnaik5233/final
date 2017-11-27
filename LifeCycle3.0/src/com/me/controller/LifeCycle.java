64de49d402463010e0919ac611c2bef7b2e744e3
Roshan Patnaik <33476175+rpatnaik5233@users.noreply.github.com>
2017-11-27 09:00:56 +0530

    Add files via upload

---
+++ LifeCycle3.0/src/com/me/controller/LifeCycle.java
@@ -0,0 +1,55 @@
+package com.me.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycle
+ */
+@WebServlet("/LifeC.do")
+public class LifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycle() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		
+		String name=request.getParameter("name");
+		String hobbies[]=request.getParameterValues("hobby");
+		out.print("hello, <b>"+name+"</b>");
+		out.print("<br/>Hobbies:");
+		out.print("<ul>");
+		for (String hobby : hobbies) {
+			out.print("<li>" + hobby);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+		
+	}
+
+}
